3fc996ef86cdbc9d47548c937d9974947fa9e6a0
June <me@june.pet>
2026-05-18 01:05:27 +0100

    try that

--- src/terraform/containers.tf
+++ src/terraform/containers.tf
@@ -105,7 +105,7 @@ resource "docker_container" "runner" {
 
   depends_on = [docker_container.docker_dind]
   restart = "unless-stopped"
-  command = ["echo", "-n", var.forgejo_runner_token, "|", "forgejo-runner", "daemon", "--url", "http://forgejo:3000/", "--uuid", var.forgejo_runner_uuid, "--token-url", "/dev/stdin", "--label", "docker:docker://node:lts"]
+  command = ["sh", "-c", "forgejo-runner daemon --url http://forgejo:3000/ --uuid ${var.forgejo_runner_uuid} --token-url <(echo -n '${var.forgejo_runner_token}') --label docker:docker://node:lts"]
 
 }
 
