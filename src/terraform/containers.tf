47d8fa02adbb9a48f0cdd9233a901a67ca951e2b
June <me@june.pet>
2026-05-19 14:07:07 +0100

    oops

--- src/terraform/containers.tf
+++ src/terraform/containers.tf
@@ -66,7 +66,7 @@ resource "docker_container" "forgejo" {
     "FORGEJO__database__NAME=forgejo",
     "FORGEJO__database__USER=forgejo",
     "FORGEJO__database__PASSWD=forgejo",
-    "FORGEJO__server_LANDING_PAGE=/explore/repos"
+    "FORGEJO__server__LANDING_PAGE=/explore/repos"
   ]
 
   depends_on = [
